ad169976feed467ac1b89f86e2b1074e7edb5ed6
f-vand <f.vand@yahoo.com>
2025-03-13 18:36:13 -0400

    3 push

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const darkModeToggle = document.getElementById("dark-mode-toggle");
   darkModeToggle.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
-    //  update button text based on mode
+    //  update button text 
     if (document.body.classList.contains("dark-mode")) {
       darkModeToggle.textContent = "Light Mode";
     } else {
@@ -32,4 +32,52 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         });
       });
+        //  contact form element
+        const form = document.querySelector("form");
+
+        // Listen  submit event on  form
+        form.addEventListener("submit", function(e) {
+          e.preventDefault(); // Prevent submission
+  
+          // Clear  error messages
+          document.getElementById("nameError").textContent = "";
+          document.getElementById("emailError").textContent = "";
+          document.getElementById("messageError").textContent = "";
+  
+          //  trim values from form
+          const name = document.getElementById("name").value.trim();
+          const email = document.getElementById("email").value.trim();
+          const message = document.getElementById("message").value.trim();
+  
+          let isValid = true;
+  
+          // Validate Name
+          if (name === "") {
+            document.getElementById("nameError").textContent = "Please enter your name.";
+            isValid = false;
+          }
+  
+          // Validate Email 
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (email === "") {
+            document.getElementById("emailError").textContent = "Please enter your email.";
+            isValid = false;
+          } else if (!emailRegex.test(email)) {
+            document.getElementById("emailError").textContent = "Please enter a valid email address.";
+            isValid = false;
+          }
+  
+          // Validate msg
+          if (message === "") {
+            document.getElementById("messageError").textContent = "Please enter your message.";
+            isValid = false;
+          }
+  
+          //   success submit.
+          if (isValid) {
+            console.log("Form submitted successfully!");
+            form.reset();
+            alert("Thank you for your message!");
+          }
+        });
 });
